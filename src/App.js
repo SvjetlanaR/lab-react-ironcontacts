72e9544497ee42d45928509cd349afd1e59915be
SvjetlanaR <svjetlanarados@gmail.com>
2021-05-05 12:26:17 +0200

    iteration 1, 2, 3

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import contacts from "./contacts.json";
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  state = {
+    myContacts: contacts.slice(0, 5),
+  };
+
+  addRandomContact = () => {
+    const randomContact =
+      contacts[Math.floor(Math.random() * contacts.length + 5)];
+    this.setState((state) => ({
+      myContacts: [randomContact, ...state.myContacts],
+    }));
+  };
+
+  sortContact = () => {
+    const sort = this.state.myContacts.sort(function (a, b) {
+      //  if (a.name < b.name ) { return -1 }
+      //  if (a.name > b.name) { return 1 }
+      //  if (a.name = b.name) { return 0 }
+      return a.name.localeCompare(b.name);
+    });
+    this.setState((state) => ({
+      myContacts: sort,
+    }));
+  };
+
+  sortPopularity = () => {
+    const sort = this.state.myContacts.sort(function (a, b) {
+      // if (a.popularity < b.popularity ) { return -1 }
+      // if (a.popularity > b.popularity) { return 1 }
+      // if (a.popularity = b.popularity) { return 0 }
+      return b.popularity - a.popularity;
+    });
+    this.setState((state) => ({
+      myContacts: sort,
+    }));
+  };
+
+  render() {
+    const ContactList = this.state.myContacts.map((contact) => (
+      <p>
+        Picture:
+        <img style={{ height: "100px" }} src={contact.pictureUrl}></img> Name:
+        {contact.name} Popularity: {contact.popularity}
+      </p>
+    ));
+
+    return (
+      <div>
+        <button onClick={this.addRandomContact}>Add Random Contact</button>
+        <button onClick={this.sortContact}>Sort Contact</button>
+        <button onClick={this.sortPopularity}>Sort Popularity </button>
+        {ContactList}
+      </div>
+    );
+  }
 }
-
-export default App;
